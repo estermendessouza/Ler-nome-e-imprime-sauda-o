9392a401d66b2ecab18ebb452f636b0bd8546eb0
Ester Mendes Souza <82113333+estermendessouza@users.noreply.github.com>
2021-12-21 16:28:09 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Programa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            Console.WriteLine("Digite seu nome");
+            nome= Console.ReadLine();
+            Console.WriteLine("Seja bem vindo(a)," + nome);
+            
+        }
+    }
+}
